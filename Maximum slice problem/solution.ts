4bed6079b0ca171fb46b948df1fb50afd9a9d229
mihnn <mihnn@tasconline.com>
2024-01-14 02:59:49 +0700

    add Maximum slice problem

---
+++ Maximum slice problem/solution.ts
@@ -0,0 +1,16 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A: number[]): number {
+    const n = A.length
+    let maxEnding = 0
+    let maxProfit = 0
+    for(let i = 0; i < n - 1; i ++) {
+        const value = A[i]
+        const nextValue = A[i + 1]
+        maxEnding = Math.max(0, maxEnding + nextValue - value)
+        maxProfit = Math.max(maxProfit, maxEnding)
+    }
+
+    return maxProfit
+}
